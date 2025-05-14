859123d8f6f9e130d976d5829b06002843c7e724
Miguel Rivas <migueladanrm@outlook.com>
2025-05-13 18:30:20 -0600

    feat: add Health module with HealthController and health checks

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotesModule } from './notes/notes.module';
 import { PrismaService } from './prisma/prisma.service';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [NotesModule],
+  imports: [NotesModule, HealthModule],
   controllers: [],
   providers: [PrismaService],
   exports: [PrismaService],
---
+++ server/src/health/health.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
+import { PrismaService } from '~/prisma';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private prismaHealth: PrismaHealthIndicator,
+    private prismaService: PrismaService,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.prismaHealth.pingCheck('prisma', this.prismaService),
+    ]);
+  }
+}
